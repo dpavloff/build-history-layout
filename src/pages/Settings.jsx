49218eddc744f99690b996ea3a8cbe2de7e89c6e
pavlov.dan <pavlov.dan@panoramik.ru>
2021-10-09 20:12:06 +0500

    Added mask, small fixes

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import '../styles/settings.css';
+
 import YandexButton from '../components/YandexButton';
+import MaskedInput from 'react-text-mask';
 
 // Redux works here ->
 
@@ -18,12 +20,14 @@ function Settings() {
       <input type="text" name="git-repo" placeholder="user-name/repo-name" required />
 
       <label htmlFor="build-command">Build command <span className="asterisk"> *</span></label>
-      <input type="text" name="build-command" required />
+      <input type="text" name="build-command" placeholder="npm install" required />
       
       <label htmlFor="branch">Main branch</label>
-      <input type="text" name="branch" required />
+      <input type="text" name="branch" placeholder="master" required />
       <div>
-        <span>Synchronize every </span> <input type="number" name="sync-min" id="sync-min" /><span> minutes</span>
+        <span>Synchronize every </span> 
+        <MaskedInput value={10} className="minute-input" mask={[/[1-9]/,/\d+/]} />
+        <span> minutes</span>
       </div>
       <div className="buttons">
         <YandexButton label={"Save"} />
