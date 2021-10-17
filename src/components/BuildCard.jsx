7ae0d92db7b76ea1745b42fd9d518950c469ce10
Daniil Pavlov <Dan43575@gmail.com>
2021-10-17 15:35:59 +0500

    Small refactoring

--- src/components/BuildCard.jsx
+++ src/components/BuildCard.jsx
@@ -29,50 +29,61 @@ function BuildCard({ buildObj }) {
   };
 
   let months = {
-      1: "янв",
-      2: "фев",
-      3: "мар",
-      4: "апр",
-      5: "мая",
-      6: "июн",
-      7: "июл",
-      8: "авг",
-      9: "сен",
-      10: "окт",
-      11: "ноя",
-      12: "дек",
-  }
+    1: "янв",
+    2: "фев",
+    3: "мар",
+    4: "апр",
+    5: "мая",
+    6: "июн",
+    7: "июл",
+    8: "авг",
+    9: "сен",
+    10: "окт",
+    11: "ноя",
+    12: "дек",
+  };
 
   return (
     <div className="build-card">
       <img src={styleObj.icon[buildObj.result]} alt={buildObj.result} />
-       <div className="card-info">
+      <div className="card-info">
         <div className="commit-info">
           <div className="build-info">
-              <h3 style={styleObj.color[buildObj.result]} className="commit-number">
+            <h3
+              style={styleObj.color[buildObj.result]}
+              className="commit-number"
+            >
               #{buildObj.commitNumber}
-              </h3>
-              <p className="commit-message">{buildObj.commitMessage}</p>
+            </h3>
+            <p className="commit-message">{buildObj.commitMessage}</p>
           </div>
           <div className="branch-info">
-              <img className="is-transparent" src={commitIcon} alt="" />
-              <p>{buildObj.branchName}</p>
-              <p className="is-transparent">{buildObj.commitHash}</p>
-              <p><img className="is-transparent" src={userIcon} alt="" />{buildObj.authorName}</p>
+            <img src={commitIcon} alt="" />
+            <p>{buildObj.branchName}</p>
+            <p>{buildObj.commitHash}</p>
+            <p>
+              <img src={userIcon} alt="" />
+              {buildObj.authorName}
+            </p>
           </div>
         </div>
-        <div className="time-info is-transparent">
-            <div className="date">
-                <img src={calendarIcon} alt="" />
-                <p> {buildObj.buildDay} {months[buildObj.buildMonth]}, {buildObj.buildTimestamp}</p>
-            </div>
-            <div className="build-time">
-                <img src={clockIcon} alt="" />
-                <p>{buildObj.buildTimeH} ч {buildObj.buildTimeM} мин</p>
-            </div>
+        <div className="time-info">
+          <div className="date">
+            <img src={calendarIcon} alt="" />
+            <p>
+              {" "}
+              {buildObj.buildDay} {months[buildObj.buildMonth]},{" "}
+              {buildObj.buildTimestamp}
+            </p>
+          </div>
+          <div className="build-time">
+            <img src={clockIcon} alt="" />
+            <p>
+              {buildObj.buildTimeH} ч {buildObj.buildTimeM} мин
+            </p>
+          </div>
         </div>
       </div>
-
     </div>
   );
 }
