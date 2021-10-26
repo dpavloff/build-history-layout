4c06591f1b2d2cd14b7ffccd574a982c1256e42c
Daniil Pavlov <Dan43575@gmail.com>
2021-10-26 17:21:34 +0500

    Added boilerplate for performance service

--- src/api/getBuildInfo.js
+++ src/api/getBuildInfo.js
@@ -1,13 +1,13 @@
-import { data } from '../mock/builds';
+import { data } from "../mock/builds";
 
 let builds = data;
 
 function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export async function getBuildsFromApi() {    
-    await timeout(1000);
+export async function getBuildsFromApi() {
+  await timeout(Math.random() * 5000);
 
-    return builds;
+  return builds;
 }
