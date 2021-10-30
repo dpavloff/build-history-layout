11f6acb93725474f15682b8517d5a8b8076bbc17
dpavloff <Dan43575@gmail.com>
2021-10-31 02:09:23 +0500

    Created test to check github actions

--- test/index.test.js
+++ test/index.test.js
@@ -2,5 +2,11 @@ describe('Sanity check', () => {
     it('Two plus two equals four', () => {
         const four = 2 + 2;
         expect(four).toEqual(4);
-    })
+    });
+
+    it('Apple is equal to apple', () => {
+        const apple = "apple";
+        expect(apple).toEqual("apple");
+    });
 });
+
